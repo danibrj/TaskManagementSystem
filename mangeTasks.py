73747bb2a2305ac474bb84ff09a14e221ca698eb
Danial Barjasteh siahkalroudy <danial.barjastehsiahkalroudy@email.kntu.ac.ir>
2026-01-05 23:34:55 +0330

    refactor(manageTask.py): Complete manage part

--- mangeTasks.py
+++ mangeTasks.py
@@ -1,24 +1,62 @@
 from Task import Task
-
+from B_Tree import BTree
+from Segment_Tree import SegmentTree
 class ManageTasks:
     def __init__(self):
-        pass
+        self.datas = BTree(2)
+        self.seg = SegmentTree(10000)
     
     def insertTask(self,t_id,startTime,endTime,value):
         task = Task(t_id,startTime,endTime,value)
-
+        self.datas.insert(task)
+        self.seg.insert_task(task.t_id,task.value)
     
     def deleteTask(self,t_id):
-        pass
+        self.datas.delete(t_id) 
+        self.seg.delete_task(t_id)
     
     def updateTask(self,t_id,startTime,endTime,value):
-        pass
+        self.datas.search_for_update(t_id,startTime,endTime,value)
+        self.seg.update_task(t_id,value)
     
     def queryTaskId(self,t_id):
-        pass
+        task = self.datas.search(t_id)
+        print(task)
     
     def queryTaskSum(self,t_id1,t_id2):
-        pass
+        print(self.seg.query_task_sum(t_id1,t_id2))
     
     def printTrees(self):
-        pass
+        self.datas.print_tree()
+    
+    
+
+# x = ManageTasks()
+# # y = Task(1,10,15,50)
+# # y2 = Task(2,15,20,80)
+# # y3 = Task(3,20,25,60)
+# # y4 = Task(4,25,30,550)
+# # y5 = Task(5,25,30,550)
+# # y6 = Task(6,25,30,550)
+# # y7 = Task(3.5,25,30,550)
+
+
+
+
+
+# x.insertTask(1,10,15,50)
+# x.insertTask(2,10,15,50)
+# x.insertTask(3,10,15,50)
+# x.insertTask(4,10,15,50)
+# x.insertTask(5,10,15,50)
+# x.insertTask(6,10,15,50)
+
+
+
+# # x.printTrees()
+# x.queryTaskId(4)
+
+
+# # x.printTrees()
+# x.queryTaskSum(2,4)
+
