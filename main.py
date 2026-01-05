2870b613cf6342702c01b0e224d44c2042c1de8a
Danial Barjasteh siahkalroudy <danial.barjastehsiahkalroudy@email.kntu.ac.ir>
2026-01-05 23:35:56 +0330

    refactor(main.py): Complete main part

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from mangeTasks import ManageTasks
+
+mT = ManageTasks()
 
 while True:
     order = input(">> ").strip().split()
@@ -9,25 +12,31 @@ while True:
         startTime = int(order[2])
         endTime = int(order[3])
         value = int(order[4])
+        mT.insertTask(t_id,startTime,endTime,value)
         
     elif command == "DeleteTask":
         t_id = int(order[1])
+        mT.deleteTask(t_id)
 
     elif command == "UpdateTask":
         t_id = int(order[1])
         startTime = int(order[2])
         endTime = int(order[3])
         value = int(order[4])
+        mT.updateTask(t_id,startTime,endTime,value)
         
     elif command == "QueryTaskId":
         t_id = int(order[1])
+        mT.queryTaskId(t_id)
         
     elif command == "QueryTaskSum":
         t_id1= int(order[1])
         t_id2 = int(order[2])
+        mT.queryTaskSum(t_id1,t_id2)
         
     elif command == "PrintTrees":
-        pass        
+        mT.printTrees()
+        
     elif command == "exit":
         break
     
