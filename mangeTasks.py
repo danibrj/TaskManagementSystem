529adfc43fdd503267daba245a685e20ab314f45
Danial Barjasteh siahkalroudy <danial.barjastehsiahkalroudy@email.kntu.ac.ir>
2026-01-05 23:30:56 +0330

    feat(manageTasks.py): create management class for project

---
+++ mangeTasks.py
@@ -0,0 +1,24 @@
+from Task import Task
+
+class ManageTasks:
+    def __init__(self):
+        pass
+    
+    def insertTask(self,t_id,startTime,endTime,value):
+        task = Task(t_id,startTime,endTime,value)
+
+    
+    def deleteTask(self,t_id):
+        pass
+    
+    def updateTask(self,t_id,startTime,endTime,value):
+        pass
+    
+    def queryTaskId(self,t_id):
+        pass
+    
+    def queryTaskSum(self,t_id1,t_id2):
+        pass
+    
+    def printTrees(self):
+        pass
